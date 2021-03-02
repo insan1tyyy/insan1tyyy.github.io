9c3fc7937f1d6e01b666bfb72682639e9b2d760f
insan1tyyy <71565965+insan1tyyy@users.noreply.github.com>
2021-03-02 05:05:40 +0300

    upd

--- assets/js/typer.js
+++ assets/js/typer.js
@@ -1,6 +1,6 @@
 $(function() {
     $(".typed").typed({
-        strings: ["dead inside", "zxc", "i choose violence", "i will remove u, kid"],
+        strings: ["dead inside", "zxc", "resting in pink grave", "i will remove u, kid", "i will fuck your sister"],
         typeSpeed: 100,
         loop: true,
         cursorChar: "|",
