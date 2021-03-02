75b0bab41425dc17747281b9de11238334906f3f
insan1tyyy <71565965+insan1tyyy@users.noreply.github.com>
2021-03-02 05:57:52 +0300

    up

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var classCycle=['imageCycle1','imageCycle2'];
-
+    var dd = 1
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
 
