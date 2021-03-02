c2075464e75bf1257dc250dff67b466628cbbc34
insan1tyyy <71565965+insan1tyyy@users.noreply.github.com>
2021-03-02 05:03:39 +0300

    upd

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,4 +1,4 @@
-var text = ["$", " $", "y $", "ty $", "1ty $", "n1ty $", "an1ty $", "san1ty $", "nsan1ty $","insan1ty $","insan1ty $","insan1t $", "insan1 $", "insan $", "insa $", "ins $", "in $", "i $"];
+var text = ["$", " $", "y $", "ty $", "1ty $", "n1ty $", "an1ty $", "san1ty $", "nsan1ty $","insan1ty $","insan1ty $","insan1t $", "insan1 $", "insan $", "insa $", "ins $", "in $", "i $", " $", "a $", "ma $", "rma $", "erma $", "perma $", "sperma $", "sperma $","sperm $","sper $","spe $", "sp $", "s $", " $", "a $", "da $", "zda $", "izda $", "pizda $", "pizda $", "pizd $", "piz $", "pi $", "p $"];
 var counter = 0
 var inst = setInterval(change, 200);
 
